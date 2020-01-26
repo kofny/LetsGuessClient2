d34b51db3e224357e4386fb6cfa836315dd17b60
Chaunecy WANG <chaunecy@outlook.com>
2020-01-26 11:43:33 +0800

    feat(#MENU): add submenu to data & task

---
+++ src/pages/data/query/index.tsx
@@ -0,0 +1,26 @@
+import {PageHeaderWrapper} from '@ant-design/pro-layout';
+// import React, { useState, useEffect } from 'react';
+// import { Spin } from 'antd';
+import styles from './index.less';
+import React from 'react';
+
+export default () => {
+  /* const [loading, setLoading] = useState<boolean>(true);
+   useEffect(() => {
+     setTimeout(() => {
+       setLoading(false);
+     }, 3000);
+   }, []);*/
+  return (
+    <PageHeaderWrapper content="在这里查看任务的状态吧!" className={styles.main}>
+      {/*      <div
+        style={{
+          paddingTop: 100,
+          textAlign: 'center',
+        }}
+      >
+        <Spin spinning={loading} size="large"></Spin>
+      </div>*/}
+    </PageHeaderWrapper>
+  );
+};
